18296b63481982ddefa3c50fe22953b9809b719f
GroovyChocobo <elizabeth8.taiwo@gmail.com>
2026-04-23 08:13:04 -0400

    added arguments to test_enter_garage_ID_not_valid

--- test_garage.py
+++ test_garage.py
@@ -2,5 +2,5 @@ import pytest
 from garage import enter_garage
 
 
-def test_enter_garage_ID_not_valid():
+def test_enter_garage_ID_not_valid(car_id, entry_hour):
     assert enter_garage("1G1AL58I5OQ123456") == 'Invalid Car ID'
