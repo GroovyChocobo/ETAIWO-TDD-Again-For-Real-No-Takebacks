cea895a51411edfdd925650ec46234c1be77a574
GroovyChocobo <elizabeth8.taiwo@gmail.com>
2026-04-23 08:09:41 -0400

    fixed test_enter_garage_ID_not_valid

--- test_garage.py
+++ test_garage.py
@@ -3,4 +3,4 @@ from garage import enter_garage
 
 
 def test_enter_garage_ID_not_valid():
-    assert enter_garage(1G1AL58I5OQ123456) == 'Invalid Car ID'
+    assert enter_garage("1G1AL58I5OQ123456") == 'Invalid Car ID'
