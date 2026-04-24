c05379b85c6485eaa9c9d085d943eee3c86a1e2d
GroovyChocobo <elizabeth8.taiwo@gmail.com>
2026-04-24 07:52:22 -0400

    fixed test_calculate fee negative hours

--- test_garage.py
+++ test_garage.py
@@ -14,14 +14,17 @@ def test_enter_garage_entry_hour_not_valid(car_id, entry_hour):
 def test_enter_garage_entry_hour_is_valid(car_id, entry_hour):
     assert enter_garage(16) == 'Valid Entry Hour'
 
+
+
 @pytest.mark.parametrize("hours, rate, expected", [1, 5, 5.0], [2, 5, 10.0], {3, 5, 10.0})
     def test_enter_garage(hours, rate, expected):
         assert enter_garage(hours, rate) == expected
 
 def test_calculate_fee_negative_hours():
-    with pytest.parametrize(ValueError)
+    with pytest.parametrize(ValueError):
         calculate_fee(-1, 5.0)
 
 def test_calculate_fee_invalid_type():
     with pytest.parametrize(TypeError)
     calculate_fee(two, 5.0)
+
